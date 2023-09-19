e6558df62fae996887ce226c8e71c30f25e853de
Chirag Jain <43720474+Chirag-Jain01@users.noreply.github.com>
2023-09-19 14:09:11 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const local = {
+    weatherKey: 'b2fd463f11325c36ba430ca3b709e14f',
+    newsKey: 'ac51f732362c472ab37cefb007e242a0',
+    movieKey: '9c11f1811e3af97a1190747a4ac6fcb6'
+}
+
+export default local;
